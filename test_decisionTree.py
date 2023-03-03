1973f0cadc12b6aacb6662fc5981daa213a30e75
RalfTischer <108927724+RalfTischer@users.noreply.github.com>
2023-03-03 10:14:11 +0000

    Tree

--- test_decisionTree.py
+++ test_decisionTree.py
@@ -16,7 +16,9 @@ y = df['Go']
 dtree = DecisionTreeClassifier()
 dtree = dtree.fit(X, y)
 
-print(dtree.predict([[40, 10, 7, 1]]))
+tree.plot_tree(dtree, feature_names=features)
+
+print(dtree.predict([[18, 2, 7, 1]]))
 
 print("[1] means 'GO'")
 print("[0] means 'NO'")
